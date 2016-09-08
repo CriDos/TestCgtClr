8482ef5992ae3bce9c301b1e56dd6bb55206987e
CriDos <CriDos@ya.ru>
2016-09-09 00:24:58 +0300

    no message

--- TestCgtClr/Program.cs
+++ TestCgtClr/Program.cs
@@ -1,15 +1,442 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CgtClr;
+using CgtClr.Share;
 
 namespace TestCgtClr
 {
+    class Cgt : BaseCgt
+    {
+        public override int _Debug(string text, int color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int _Error(int line, int id_element, string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int arrCount(int id_value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int arrGetItem(int id_value, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int arrItemData(int id_value, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string arrItemName(int id_value, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DataType arrType(int id_value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int dtInt(int id_value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double dtReal(int id_value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string dtStr(int id_value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DataType dtType(int id_value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ElementClass elGetClassIndex(int id_element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string elGetClassName(int id_element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string elGetCodeName(int id_element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int elGetData(int id_element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int elGetEID(int id_point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ElementFlags elGetFlag(int id_element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int elGetGroup(int id_element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string elGetInfSub(int id_element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string elGetInherit(int id_element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string elGetInterface(int id_element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int elGetParent(int id_element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void elGetPos(int id_element, ref int X, ref int Y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int elGetPropCount(int id_element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int elGetProperty(int id_element, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int elGetPropertyListCount(int id_element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int elGetPropertyListItem(int id_element, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int elGetPt(int id_element, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int elGetPtCount(int id_element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int elGetPtName(int id_element, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int elGetSDK(int id_element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int elGetSDKByIndex(int id_element, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int elGetSDKCount(int id_element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string elGetSDKName(int id_element, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void elGetSize(int id_element, ref int W, ref int H)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool elIsDefProp(int id_element, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool elLinkIs(int id_element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int elLinkMain(int id_element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int elSetCodeName(int id_element, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void elSetData(int id_element, int data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte fntCharSet(int id_value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override uint fntColor(int id_value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string fntName(int id_value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int fntSize(int id_value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte fntStyle(int id_value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object GetParam(CgtParams index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int isDebug(int id_element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string plGetGroup(int id_point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string plGetInfo(int id_point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string plGetName(int id_point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int plGetOwner(int id_prop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int plGetProperty(int id_point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int propGetLinkedElement(int id_prop, string propName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int propGetLinkedElementInfo(int id_element, int id_prop, string info)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string propGetName(int id_prop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DataType propGetType(int id_prop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int propGetValue(int id_prop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int propIsTranslate(int id_element, int id_prop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int propSaveToFile(int id_prop, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte propToByte(int id_prop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int propToInteger(int id_prop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double propToReal(int id_prop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string propToString(int id_prop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string pt_dpeGetName(int id_point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DataType ptGetDataType(int id_point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int ptGetIndex(int id_point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ptGetInfo(int id_prop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int ptGetLinkPoint(int id_point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ptGetName(int id_point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int ptGetParent(int id_point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int ptGetRLinkPoint(int id_point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override PointType ptGetType(int id_point)
+        {
+            throw new NotImplementedException();
+        }
+        public override string resAddBitmap(int id_prop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int resAddFile(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string resAddIcon(int id_prop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string resAddMenu(int id_prop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string resAddStr(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string resAddStream(int id_prop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string resAddWave(int id_prop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int resEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int resSetPref(string pref)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int sdkGetCount(int id_sdk)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int sdkGetElement(int id_sdk, int index)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public override int sdkGetElementName(int id_sdk, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int sdkGetParent(int id_sdk)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
